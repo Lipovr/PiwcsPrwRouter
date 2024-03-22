cd4bd6c0c0d1ac07b7c756610cd08101452266b5
Roman Lipov <lipov.ra@phystech.edu>
2024-03-22 21:21:53 +0300

    Stub implementation of main function
    - Add outline of main

---
+++ src/route/main.cpp
@@ -0,0 +1,26 @@
+#include "route/router.h"
+
+
+/*
+ * TODO Stub WIP
+ */
+namespace piwcs::prw::router{
+int main(int argc, char* argv){
+
+// TODO Initialize input stream
+
+	Model in_model = Model();
+
+// TODO Read Model from input stream
+
+	RouteTables in_tables = AlgWorkspace::makeRouteTables(in_model);
+
+// TODO Cull routing tables
+
+// TODO Initialize output stream with resulting routing tables
+
+// TODO Send output stream to specified output
+
+	return 0;
+}
+}
