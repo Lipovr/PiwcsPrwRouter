Lipovr/PiwcsPrwRouter

commit 5c99a8c4435ca6621a61b69de16d00368d2c997e

Added headers
First revision of headers.

include/routetables.h
@@ -1,0 +1,47 @@
+#ifndef INCLUDE_ROUTETABLES_H_
+#define INCLUDE_ROUTETABLES_H_
+
+#include<map>
+#include<iostream>
+
+#include "util.h"
+
+namespace piwcs::prw::router{
+
+enum class TableNodeType{
+	DESTINATION,
+	MOTORIZED,
+	OTHER,
+};
+
+struct TableNode{
+	/**
+	 * <Origin Id, {exit, distance}>
+	 */
+	std::unordered_map<Identifier, RouteTableItem> table;
+	TableNodeType type;
+	Identifier dadress{};
+	Identifier dname{};
+
+};
+
+class RouteTables{
+public:
+	/*
+	 * <Destination Id, TableNode>
+	 */
+	std::unordered_map<Identifier, TableNode>;
+
+	/*
+	 * Output routing data to std::ostream in .csv format
+	 */
+
+	friend std::ostream& operator<<(std::ostream &os, const RouteTables& tables);
+
+	friend std::istream& operator>>(std::istream &is, RouteTables& tables);
+
+
+};
+
+}
+#endif /* INCLUDE_ROUTETABLES_H_ */

include/common.h
@@ -1,0 +1,10 @@
+#ifndef INCLUDE_COMMON_H_
+#define INCLUDE_COMMON_H_
+
+#ifdef NDEBUG
+#define INSPECTABLE private
+#else
+#define INSPECTABLE public
+#endif
+
+#endif /* INCLUDE_COMMON_H_ */

include/fwd.h
@@ -1,0 +1,18 @@
+#ifndef INCLUDE_FWD_H_
+#define INCLUDE_FWD_H_
+
+namespace piwcs::prw::router{
+	struct RouteNeighbor;
+	struct RouteTableItem;
+
+	class RouteTables;
+	class AlgWorkspace;
+	class RouteNode;
+	class HeapElement;
+	class RouteDistHeap;
+
+}
+
+
+
+#endif /* INCLUDE_FWD_H_ */

include/route/distheap.h
@@ -1,0 +1,26 @@
+#ifndef INCLUDE_ROUTE_DISTHEAP_H_
+#define INCLUDE_ROUTE_DISTHEAP_H_
+
+#include<queue>
+
+#include "util.h"
+
+namespace piwcs::prw::router{
+
+	class HeapElement{
+	public:
+		const Length routeLength;
+		const Index current_i;
+		const Index previous_i;
+
+		//Length operator<=>(const HeapElement &rhs) const {return this.routeLength - rhs.routelength};
+		bool operator<(const HeapElement &rhs) const { return this->routeLength < rhs.routeLength; }
+		bool operator<=(const HeapElement &rhs) const { return this->routeLength <= rhs.routeLength; }
+		bool operator>(const HeapElement &rhs) const { return this->routeLength > rhs.routeLength; }
+		bool operator>=(const HeapElement &rhs) const { return this->routeLength >= rhs.routeLength; }
+		bool operator==(const HeapElement &rhs) const { return this->routeLength == rhs.routeLength; }
+	};
+
+	using DistHeap = std::priority_queue<HeapElement>;
+}
+#endif /* INCLUDE_ROUTE_DISTHEAP_H_ */

include/route/node.h
@@ -1,0 +1,65 @@
+#ifndef INCLUDE_ROUTE_NODE_H_
+#define INCLUDE_ROUTE_NODE_H_
+#include <vector>
+
+#
+#include "util.h"
+
+namespace piwcs::prw::router{
+
+enum class NodeStatus {
+		NOT_VISITED,
+		VISITED,
+	};
+
+/**
+ * Structure with information on neighboring node
+ * in adjacency list representation.
+ *
+ * @note A node 'B' is considered a neighbor to another node 'A' if they are
+ * connected with a section and 'B' be reached from 'A'.
+ * Thus if 'A' is connected to 'B' with unidirectional
+ * section 'A'->'B' 'B' is a neighbor of 'A', but 'A' is not a neighbor of 'B'
+ * since it can't be reached.
+ */
+struct RouteNeighbor{
+	Index node_i;
+	Length distance;
+};
+
+class RouteNode{
+  INSPECTABLE:
+	Identifier m_id;
+	std::vector<RouteNeighbor> m_neighbors;
+	std::vector<RouteTableItem> m_table;
+	NodeStatus m_visitstatus;
+	bool m_isdestination;
+
+  public:
+
+	RouteNode(const Identifier &id);
+
+	const Identifier &id() const { return m_id; }
+
+	NodeStatus visitStatus() const { return m_visitstatus; }
+
+	void markVisited() { m_visitstatus = NodeStatus::VISITED; }
+	/*
+	 * Reserve memory for node's routing table to hold
+	 * all possible destinations (all other nodes in graph)
+	 */
+	void ensureTableCapacity(Index N);
+	/*
+	 * Create new record in node's routing table. If record for this
+	 * node already exists*
+	 */
+	bool insertTableRecord(Index destination, Index exit, Length distance);
+
+	void addNeighbor(Index index, Length distance);
+
+
+};
+
+}
+
+#endif /* INCLUDE_ROUTE_NODE_H_ */

include/route/router.h
@@ -1,0 +1,84 @@
+#ifndef INCLUDE_ROUTE_ROUTER_H_
+#define INCLUDE_ROUTE_ROUTER_H_
+
+#include <vector>
+#include <memory>
+
+#include<piwcsprwmodel.h> //#TODO Unnecessary?
+
+#include "common.h"
+#include "util.h"
+#include "routetables.h"
+#include "distheap.h"
+#include "node.h"
+
+
+namespace piwcs::prw::router{
+
+/**
+ * Class containing properties of nodes in exploded graph
+ */
+class AlgWorkspace{
+  public:
+
+	/*
+	 * TODO add comment
+	 */
+	static RouteTables makeRouteTables(const Model &InModel);
+
+#ifndef NDEBUG
+
+	/**
+	 * Functions to populate workspace manually.
+	 * For debug and tests only
+	 */
+
+	/**
+	 * Create empty workspace
+	 */
+	static std::unique_ptr<AlgWorkspace> d_makeDebugWorkspace();
+
+	/*
+	 * Adds a new RouteNode to workspace
+	 *
+	 * @return index of newly inserted node
+	 */
+	Index d_addNode(const Identifier &id);
+
+	/*
+	 * Adds new unidirectional section to workspace
+	 */
+	void d_addSection(Index from, Index to, Length distance);
+#endif // NDEBUG
+
+  INSPECTABLE:
+	std::vector<RouteNode> m_nodes;
+	std::vector<bool> m_nodeinheap;
+	DistHeap m_heap;
+
+	//bool m_routed;
+
+	AlgWorkspace(const std::vector<RouteNode> &nodes);
+	
+	/*
+	 * Process information from Model and generate AlgWorkspace containing
+	 * information on graph in adjacency list form
+	 */
+	static std::unique_ptr<AlgWorkspace> m_makeWorkspace(const Model &InModel);
+
+	/*
+	 * For each node in workspace Run Dijekstra algorithm
+	 * and generate routing table
+	 */
+	void m_run();
+
+	/*
+	 * Collect routing tables from nodes where it is necessary and
+	 * generate RouteTables object
+	 */
+	RouteTables m_getTables();
+
+};
+
+}
+#endif /* INCLUDE_ROUTE_ROUTER_H_ */

include/util.h
@@ -1,0 +1,35 @@
+#ifndef INCLUDE_UTIL_H_
+#define INCLUDE_UTIL_H_
+
+#include<piwcsprwmodel.h>
+
+
+namespace piwcs::prw::router{
+
+/**
+ * Type alias for route distance and section lengths.
+ * Inherited from prwmodel
+ */
+using Length = piwcs::prw::Section::Length;
+
+/**
+ * Type alias for identifiers. Effectively a string.
+ * Inherited from prwmodel
+ */
+using Identifier = piwcs::prw::Identifier;
+
+using Index = size_t;
+
+struct RouteTableItem{
+	static constexpr Index NOT_COMPUTED = ~0;
+	static constexpr Length INF = ~0;
+
+	Index exitIdx = NOT_COMPUTED;
+	Length routeLength = INF;
+
+	bool isComputed() const {return exitIdx != NOT_COMPUTED;}
+
+};
+
+}
+#endif /* INCLUDE_UTIL_H_ */

commit 8414c32d11b1ac8bdb00228966ac862a5397088a

Fixed error and renamed RouteTables members
- Fix error in RouteTables members declaration introduced in 5c99a8c4435ca6621a61b69de16d00368d2c997e
- Rename RouteTables::table to RouteTables::origins
- Fix wrong header inclusion

include/routetables.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_ROUTETABLES_H_
 #define INCLUDE_ROUTETABLES_H_
 
-#include<map>
+#include<unordered_map>
 #include<iostream>
 
 #include "util.h"
@@ -18,7 +18,7 @@ struct TableNode{
 	/**
 	 * <Origin Id, {exit, distance}>
 	 */
-	std::unordered_map<Identifier, RouteTableItem> table;
+	std::unordered_map<Identifier, RouteTableItem> origins;
 	TableNodeType type;
 	Identifier dadress{};
 	Identifier dname{};
@@ -30,7 +30,7 @@ public:
 	/*
 	 * <Destination Id, TableNode>
 	 */
-	std::unordered_map<Identifier, TableNode>;
+	std::unordered_map<Identifier, TableNode> destinations;
 
 	/*
 	 * Output routing data to std::ostream in .csv format