fa8e1d2e60febe53ad8a846f3d421b3193f8a9cc
Roman Lipov <lipov.ra@phystech.edu>
2024-03-23 17:28:02 +0300

    Defined consts namespace
    - Split aliases into separate block
    - Define piwcs::prw::router::consts namespace
    - Move RouteTableItem::INF to new namespace
    - Add SELF constant
    - Add comments

--- include/util.h
+++ include/util.h
@@ -2,34 +2,49 @@
 #define INCLUDE_UTIL_H_
 
 #include<piwcsprwmodel.h>
-
-
 namespace piwcs::prw::router{
 
 /**
  * Type alias for route distance and section lengths.
- * Inherited from prwmodel
+ * Inherited from piwcsprwmodel
  */
 using Length = piwcs::prw::Section::Length;
 
 /**
  * Type alias for identifiers. Effectively a string.
- * Inherited from prwmodel
+ * Inherited from piwcsprwmodel
  */
 using Identifier = piwcs::prw::Identifier;
-
+/**
+ * Type alias for indexes.
+ */
 using Index = size_t;
+}
+
+namespace piwcs::prw::router::consts{
+	/**
+	 * Constant for infinite length of a route (e.g. destination unreachable)
+	 */
+	constexpr Length INF = ~0;
+	/**
+	 * Constant for length of route from a node to itself
+	 */
+	constexpr Length SELF = 0;
+}
+
+namespace piwcs::prw::router{
 
 struct RouteTableItem{
 	static constexpr Index NOT_COMPUTED = ~0;
-	static constexpr Length INF = ~0;
 
 	Index exitIdx = NOT_COMPUTED;
-	Length routeLength = INF;
+	Length routeLength = consts::INF;
 
 	bool isComputed() const {return exitIdx != NOT_COMPUTED;}
 
 };
+
+
 
 }
 #endif /* INCLUDE_UTIL_H_ */
