341cc2e86f50f0bf9ca658f7b26441d18b3e75f7
Roman Lipov <lipov.ra@phystech.edu>
2024-03-27 19:59:20 +0300

    Continued implementing AlgWorkspace
    - Continue to implement core routing logic

--- src/route/router.cpp
+++ src/route/router.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include<iostream>
+#include<format>
 
 #include "route/router.h"
 
@@ -37,24 +38,93 @@ void AlgWorkspace::m_run(){
 		if (!m_nodes[origin_i].requiresRouting()){
 			continue;
 		}
-
+		RouteNode& origin = m_nodes[origin_i];
 		/*
 		 *  Reset workspace variables
 		 */
-		for (auto it = m_nodeinheap.begin(); it != m_nodeinheap.end(); it++){
-			*it = false;
+		m_reset();
+
+		/*
+		 * Initiate Dijkstra's algorithm by adding origin with
+		 * zero distance to itself
+		 *
+		 * We also assume that previous node of route from origin to itself
+		 * is origin. Therefore additional check is needed to determine if
+		 * we search for this route to add it to routing table as 'SELF'
+		 */
+		m_heap.add(HeapItem(0, origin_i, origin_i));
+
+		for(; !m_heap.empty();){
+			Index current_i = m_heap.top().current_i;
+			Index previous_i = m_heap.top().previous_i;
+
+			RouteNode& current = m_nodes[current_i];
+
+			/************************************************/
+			/* Iterate over current's neighbors             */
+			/************************************************/
+			for(Index exit=0; exit<current.neighbourCount(); ++exit){
+				// Skip neighbor if visited
+				if(NodeStatus::VISITED==
+						m_nodevisited[current.getNeighbor(exit).node_i]){
+					continue;
+				}
+
+				RouteNeighbor& neighbor_p = current.getNeighbor(exit);
+				Index neighbor_i = neighbor_p.node_i;
+				RouteNode& neighbor = m_nodes[neighbor_i];
+				Length dist_curr = m_heap.dist(current_i)+neighbor_p.distance;
+
+				/*
+				 * No need to check if neighbor is in heap
+				 * because in this case its route length will be INF
+				 */
+				if(dist_curr<m_heap.dist(neighbor_i)){
+						m_heap.add(HeapItem(dist_curr, neighbor_i, current_i));
+				}
+			}
+
+			/************************************************/
+			/* Modify origin's routing table                */
+			/************************************************/
+			Length distance = consts::INF;
+			Index exit = RouteTableItem::NOT_COMPUTED;
+			if(current_i==origin_i){
+				exit = RouteTableItem::NOT_COMPUTED;
+				distance = consts::SELF;
+			}
+			else{
+
+				if(previous_i==origin_i){
+					// Current is neighbor of origin
+					exit = origin.findNeighbor(current_i);
+					if(RouteNode::NOT_FOUND==exit){
+						throw std::format(
+								"Neighbor with index '{}' not found at node with index '{}'",
+								current_i, origin_i);
+					}
+					distance = origin.getNeighbor(exit).distance;
+				}
+				else{
+					/*
+					 *  Current is not neighbor of origin
+					 *  and previous has record in table
+					 *
+					 *  Optimal route to current lies through previous
+					 */
+					RouteTableItem& previous_rec = origin.getTableItem(previous_i);
+					exit = previous_rec.exitIdx;
+					distance = m_heap.dist(current_i);
+				}
+			}
+			origin.insertTableRecord(current_i, exit, distance);
+
+			/************************************************/
+			/* Mark current visited                         */
+			/************************************************/
+			m_nodevisited[current_i] = NodeStatus::VISITED;
+
 		}
-
-
-		if (!m_heap.empty()){
-			// "It will never happen"
-			#ifdef NDEBUG
-			std::cout << "Heap is not empty, emptying" << "/n";
-			#endif /* NDEBUG */
-			// Empty the heap
-			for (; !m_heap.empty(); m_heap.pop()){}
-		}
-
 
 	}
 }
