6f0846dd8f5503e4bfae17082a463927a53c3321
Roman Lipov <lipov.ra@phystech.edu>
2024-04-12 13:43:59 +0300

    Added tests for AlgWorkspace debug functions

--- tests/algWorkspace.cpp
+++ tests/algWorkspace.cpp
@@ -1,18 +1,37 @@
+#include <new>
+
 #include <gtest/gtest.h>
 
 #include "route/router.h"
 
 using namespace piwcs::prw::router;
-
-class AlgWorkspaceTests : public testing::Test{
+TEST(AlgWorkspaceBasicTests, DebugMakeWorkspace){
+	std::unique_ptr<AlgWorkspace> workspaceptr = std::move(AlgWorkspace::d_makeDebugWorkspace());
+	ASSERT_EQ(workspaceptr->m_nodes.size(), 0);
+	ASSERT_EQ(workspaceptr->m_nodeCount, 0);
+}
+auto nodes_t = std::vector<RouteNode>();
+class AlgWorkspaceDebugTests : public testing::Test{
 public:
-	auto workplaceptr = AlgWorkspace::d_makeDebugWorkspace();
+	std::unique_ptr<AlgWorkspace> workspaceptr = AlgWorkspace::d_makeDebugWorkspace();
 	Identifier id_0 = "this_is_id_0";
+	Identifier id_1 = "this_is_id_0";
+	Index N = 10; // node count
 };
 
-TEST_F(AlgWorkspaceTests, DebugAdd){
-	workplaceptr->d_addNode(id_0, false, true);
-	ASSERT_EQ(workplaceptr->m_nodes[0], )
+TEST_F(AlgWorkspaceDebugTests, DebugAddNode){
+	workspaceptr->d_addNode(id_0, N, true, false);
+	ASSERT_EQ(workspaceptr->m_nodes[0].id(), id_0);
+	ASSERT_EQ(workspaceptr->m_nodes[0].neighbourCount(), 0);
+	ASSERT_TRUE(workspaceptr->m_nodes[0].requiresRouting());
+	ASSERT_FALSE(workspaceptr->m_nodes[0].isDestination());
+}
 
+TEST_F(AlgWorkspaceDebugTests, DebugAddSection){
+	workspaceptr->d_addNode(id_0, N, true, false);
+	workspaceptr->d_addNode(id_1, N, true, false);
+	workspaceptr->d_addSection(0, 1, 10);
+	Index ne_idx = workspaceptr->m_nodes[0].findNeighbor(1);
+	ASSERT_EQ(workspaceptr->m_nodes[0].getNeighbor(ne_idx), RouteNeighbor(1, 10));
 }
 
