ec0b4ed9d5c3d5492e174725b9dd91da9c61f361
Roman Lipov <lipov.ra@phystech.edu>
2024-07-09 16:41:55 +0300

    Implemented `ModelParser::getNodeInfo`

--- src/route/modelparser.cpp
+++ src/route/modelparser.cpp
@@ -43,4 +43,78 @@ const Node *ModelParser::resolveSection(IdRef _section_id, IdRef _from_id) {
                       _section_id, _from_id));
 }
 
+std::vector<ModelParser::RegularNodeInfo> ModelParser::getNodeInfo(IdRef _id) {
+    const Node *node = m_model.node(_id);
+    _ASSERT(node, std::format("Invalid node id encountered: \"%s\"", _id));
+
+    NodeType type = node->type();
+    std::vector<RegularNodeInfo> result;
+    result.reserve(2);
+    auto sections = std::vector<const Section *>(node->sectionCount(), nullptr);
+
+    /* Check section list for nullptrs
+     * (this should succeed on a correct model)
+     */
+    for (SlotId slot = 0; slot < node->sectionCount(); slot++) {
+        sections[slot] = m_model.section(node->section(slot));
+        _ASSERT(sections[slot],
+                std::format("Invalid section id encountered: \"%s\" in node "
+                            "\"%s\" section \"%s\"",
+                            node->section(slot)));
+    }
+
+    /*
+     * Construct regular nodes
+     *
+     * This is a trivial implementation which assigns every slot that can be
+     * entered (labeled `start`) to a separate regular node. Constructed
+     * regular node can be exited through sections in slots reachable from
+     * `start` one.
+     */
+    for (SlotId start = 0; start < node->sectionCount(); start++) {
+
+        bool isBidirectional = sections.at(start)->isBidir();
+        bool isUnidirectional = sections.at(start)->isUnidir();
+        bool isSectionEnd = sections.at(start)->end() == _id;
+
+        // If slot cannot be entered it doesn't generate a regular node
+        if (isBidirectional || (isUnidirectional && isSectionEnd)) {
+            RegularNodeInfo thisResult;
+            /*
+             * Fill in inward slots leading to regular node
+             *
+             * In this case of trivial regular node construction every
+             * regular node corresponds to a single `start` slot
+             */
+            thisResult.slots.push_back(start);
+
+            /*
+             * Fill in sections leading from this regular node
+             *
+             * Take every section that can be reached from `start`
+             */
+            for (SlotId end = 0; end < node->sectionCount(); end++) {
+                if (node->couldTraverse(start, end)) {
+                    thisResult.sections.push_back(sections.at(end)->id());
+                }
+            }
+
+            // Fill in regular node metadata
+            for (auto routedType : {MOTORIZED}) {
+                if (type == routedType) {
+                    thisResult.meta.requires_routing = true;
+                    break;
+                }
+            }
+
+            thisResult.meta.id_model = node->id();
+
+            // Push current regular node to result
+            result.push_back(std::move(thisResult));
+        }
+    }
+
+    return std::move(result);
+}
+
 } // namespace piwcs::prw::router
