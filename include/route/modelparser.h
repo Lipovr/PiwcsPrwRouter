1d9cd94dc71905cd44a70d886d4548843d52af15
Roman Lipov <lipov.ra@phystech.edu>
2024-07-09 16:27:52 +0300

    Reformatted with clang-format

--- include/route/modelparser.h
+++ include/route/modelparser.h
@@ -1,62 +1,60 @@
 #ifndef INCLUDE_ROUTE_MODELPARSER_H_
 #define INCLUDE_ROUTE_MODELPARSER_H_
 
-#include<map>
+#include <map>
 
 #include "common.h"
 #include "fwd.h"
 #include "util.h"
 
-namespace piwcs::prw::router{
+namespace piwcs::prw::router {
 
-struct ParseResult{
+struct ParseResult {
 
-	Graph graph;
-	const GraphMetadata meta;
+    Graph graph;
+    const GraphMetadata meta;
 
 }; // struct ParseResult
 
-class ModelParser{
-public:
+class ModelParser {
+  public:
+    using ModelRef = const Model &;
+    using ParseMap = IdMap<std::vector<Index>>;
+    static constexpr Index INDEX_INVALID = ~0;
 
-	using ModelRef = const Model&;
-	using ParseMap = IdMap<std::vector<Index>>;
-	static constexpr Index INDEX_INVALID = ~0;
+    struct ModelSectionMapping {
+        Identifier id;
+        SlotId slot;
+    };
 
-	struct ModelSectionMapping{
-	    Identifier id;
-	    SlotId slot;
-	};
-
-	struct RegNodeMeta{
+    struct RegNodeMeta {
         bool requires_routing = false;
         Identifier id_model = ID_NULL;
     };
 
-    struct RegularNodeInfo{
+    struct RegularNodeInfo {
         std::vector<Identifier> sections;
         std::vector<SlotId> slots;
         RegNodeMeta meta;
     };
 
-	/*
-	 * Construct new ModelParser
-	 */
-	ModelParser(ModelRef _model);
+    /*
+     * Construct new ModelParser
+     */
+    ModelParser(ModelRef _model);
 
-	ModelParser() = delete;
+    ModelParser() = delete;
 
-	ParseResult parse() const;
+    ParseResult parse() const;
 
-INSPECTABLE:
+    INSPECTABLE :
 
-	ModelRef m_model;
-	ParseMap m_pmap;
+        ModelRef m_model;
+    ParseMap m_pmap;
 
-	std::vector<RegularNodeInfo> getNodeInfo(IdRef _id);
+    std::vector<RegularNodeInfo> getNodeInfo(IdRef _id);
 
-	const Node* resolveSection(IdRef _section_id, IdRef _from_id);
-
+    const Node *resolveSection(IdRef _section_id, IdRef _from_id);
 
 }; // class ModelParser
 
--- src/route/modelparser.cpp
+++ src/route/modelparser.cpp
@@ -1,47 +1,46 @@
 #include <format>
 
 #include "debug.h"
-#include"route/modelparser.h"
+#include "route/modelparser.h"
 
+namespace piwcs::prw::router {
 
-namespace piwcs::prw::router{
+ModelParser::ModelParser(ModelRef _model)
+    : m_model(_model), m_pmap(ParseMap()) {}
 
-	ModelParser::ModelParser(ModelRef _model):
-			  m_model(_model)
-			, m_pmap(ParseMap())
-	{}
+ParseResult ModelParser::parse() const {
+    ParseMap pmap;
+    for (auto &[id, node] : m_model.nodes()) {
+        bool ins_result = pmap.insert(
+            {id, std::vector<Index>(node.sectionCount(), INDEX_INVALID)});
+    }
+}
 
-	ParseResult ModelParser::parse() const{
-		ParseMap pmap;
-		for (auto& [id, node]: m_model.nodes()) {
-			bool ins_result = pmap.insert({id, std::vector<Index>(node.sectionCount(), INDEX_INVALID)});
+const Node *ModelParser::resolveSection(IdRef _section_id, IdRef _from_id) {
+    const Node *start = m_model.node(_from_id);
+    const Section *section = m_model.section(_section_id);
 
-		}
-	}
+    _DEBUG_ONLY() if (!start) {
+        _FAIL(std::format("Node with identifier \"%s\" not found", _from_id));
+    }
 
-	const Node* ModelParser::resolveSection(IdRef _section_id, IdRef _from_id){
-		const Node* start = m_model.node(_from_id);
-		const Section* section = m_model.section(_section_id);
+    _DEBUG_ONLY() if (!section) {
+        _FAIL(std::format("Section with identifier \"%s\" not found",
+                          _section_id));
+    }
 
-		_DEBUG_ONLY() if (!start){
-			_FAIL(std::format("Node with identifier \"%s\" not found", _from_id));
-		}
+    if (section->node(0) == _from_id) {
+        // from is at slot 0 for this section
+        return m_model.node(section->node(1));
+    }
 
-		_DEBUG_ONLY() if (!section){
-			_FAIL(std::format("Section with identifier \"%s\" not found", _section_id));
-		}
+    if (section->node(1) == _from_id) {
+        // from is at slot 1 for this section
+        return m_model.node(section->node(0));
+    }
 
-		if (section->node(0)==_from_id){
-			// from is at slot 0 for this section
-			return m_model.node(section->node(1));
-		}
-
-		if (section->node(1)==_from_id){
-			// from is at slot 1 for this section
-			return m_model.node(section->node(0));
-		}
-
-		_FAIL(std::format("Section \"%s\" not connected to node \"%s\"", _section_id, _from_id));
-	}
+    _FAIL(std::format("Section \"%s\" not connected to node \"%s\"",
+                      _section_id, _from_id));
+}
 
 } // namespace piwcs::prw::router
